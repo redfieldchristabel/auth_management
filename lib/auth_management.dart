c90a9e87f849e66a6a93814335a34c6968f6f1c5
imano00 <nurimanjamal00@gmail.com>
2023-05-03 16:01:10 +0800

    add example

--- lib/auth_management.dart
+++ lib/auth_management.dart
@@ -1,4 +1,4 @@
 library auth_management;
 
 export 'models/base_user.dart';
-
+export 'widgets/auth_screen_handler.dart';
---
+++ lib/models/base_user.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'base_user.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userNotifierHash() => r'a2ec8d7bc82b12477f67611d01ecf3c78baf5a91';
+
+/// See also [UserNotifier].
+@ProviderFor(UserNotifier)
+final userNotifierProvider =
+    AutoDisposeNotifierProvider<UserNotifier, BaseUser?>.internal(
+  UserNotifier.new,
+  name: r'userNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserNotifier = AutoDisposeNotifier<BaseUser?>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
