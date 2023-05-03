04e904cd791ec043db5723d8cb6ef4de0954cf77
imano00 <nurimanjamal00@gmail.com>
2023-05-03 16:00:47 +0800

    add example

--- lib/models/base_user.dart
+++ lib/models/base_user.dart
@@ -1,7 +1,33 @@
+import 'dart:ui';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'base_user.g.dart';
+
 abstract class BaseUser {
   final String id;
   final String username;
   final String email;
 
   BaseUser({required this.id, required this.username, required this.email});
+}
+
+@riverpod
+class UserNotifier extends _$UserNotifier {
+  @override
+  BaseUser? build() {
+    return null;
+  }
+
+  logIn(BaseUser user) {
+    state = user;
+  }
+
+  void logOut() {
+    state = null;
+  }
+
+  update(BaseUser user){
+    state = user;
+  }
 }
--- lib/widgets/auth_screen_handler.dart
+++ lib/widgets/auth_screen_handler.dart
@@ -1,15 +1,6 @@
 import 'package:auth_management/models/base_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'auth_screen_handler.g.dart';
-
-@riverpod
-BaseUser? user(UserRef ref) {
- return null;
-}
-
 
 class AuthScreenHandler extends ConsumerWidget {
   const AuthScreenHandler({required this.authScreen, required this.afterAuthScreen, Key? key}) : super(key: key);
@@ -19,6 +10,6 @@ class AuthScreenHandler extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref.watch(userProvider) != null ? afterAuthScreen: authScreen;
+    return ref.watch(userNotifierProvider) != null ? afterAuthScreen: authScreen;
   }
 }
