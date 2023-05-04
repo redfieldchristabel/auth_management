e5729989322e6d687eb25f07608e2c7b279f11e9
redfield <redfieldchristabel@gmail.com>
2023-05-04 17:36:20 +0800

    add documentation

--- lib/widgets/auth_screen_handler.dart
+++ lib/widgets/auth_screen_handler.dart
@@ -3,6 +3,13 @@ import 'package:auth_management/services/base_auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// A widget that handles authentication state and switches between two screens based on whether a user is authenticated or not.
+///
+/// [authScreen] The screen to display when a user is not authenticated.
+/// [afterAuthScreen] The screen to display after a user is authenticated.
+/// [authService] The authentication service to use. Normally the class are named by "AuthService"
+/// and the class is must be extended from [BaseAuthService].
+/// use Mason command "mason make auth_service to generate this service file.
 class AuthScreenHandler<T extends BaseUser> extends ConsumerStatefulWidget {
   const AuthScreenHandler(
       {required this.authService,
