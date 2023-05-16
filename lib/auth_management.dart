5d21c802f5db004ea9dcde3e72f18f9dde10610a
redfield <redfieldchristabel@gmail.com>
2023-05-16 13:22:52 +0800

    export some class

--- lib/auth_management.dart
+++ lib/auth_management.dart
@@ -3,3 +3,5 @@ library auth_management;
 export 'models/base_user.dart';
 export 'widgets/auth_screen_handler.dart';
 export 'auth_management_exception.dart';
+export 'services/base_auth_service.dart';
+export 'services/firebase_auth_service.dart';
